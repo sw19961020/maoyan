e37106eaf97e12949b4437a00f782409d5a82720
宋伟 <1357875122@qq.com>
2020-10-10 16:46:53 +0800

    初步完成city页面

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import showModule from './modules/show'
+import showcityModule from './modules/showcity'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -11,6 +12,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-    showModule
+    showModule,
+    showcityModule
   }
 })
